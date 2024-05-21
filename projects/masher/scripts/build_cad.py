58a2894aebf7af5cded7ce2c4773febc3fe3d5ef
lmpriestley <lmpriestley@outlook.com>
2024-05-20 21:00:22 -0400

    Scaffold script for building meshes

--- projects/masher/scripts/build_cad.py
+++ projects/masher/scripts/build_cad.py
@@ -1,10 +1,73 @@
 """
 Python script for building mesh models from a step file
+
+Uses the `FreeCAD` python API to build a mesh model from a step file.
 """
+
+import sys
+import os
+
+messages = {
+    "usage": "Usage: python build_cad.py <output_file>",
+}
 
 
 def main():
-    pass
+    if len(sys.argv) < 3:
+        print(messages["usage"])
+        return
+
+    input_file = "case.step"
+    output_file = sys.argv[1]
+
+    init_build_dir(output_file)
+
+
+def init_build_dir(output_file):
+    """Initialize the build directory to a known state
+
+    Args:
+        output_file (str): Desired path of the output file.
+
+    Returns:
+        IOError: If the output file is invalid.
+        None: If the output file is valid.
+    """
+    out_valid = validate_outfile(output_file)
+    if out_valid is None:
+        maybe_makedir(output_file)
+    elif out_valid is IOError:
+        print(f"Invalid output file: {output_file}")
+        print(messages["usage"])
+        return out_valid
+
+
+def maybe_makedir(path):
+    """Creates a directory for a file.
+
+    Does nothing if `path` is a directory.
+
+    Args:
+        path (str): Path to a file to maybe create parents for.
+    """
+    # TODO(Lucas): Validate the path should be made
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+
+
+def validate_outfile(path):
+    """Vaidate the user specified output file.
+
+    Args:
+        path (str): Path to the output file.
+
+    Returns:
+        None: If the outfile is valid.
+        IOError: If the outfile is invalid.
+    """
+    if isinstance(path, str):
+        return
+    elif os.path.isdir(path):
+        return IOError(f"Output file is a directory: {path}")
 
 
 if __name__ == "__main__":
