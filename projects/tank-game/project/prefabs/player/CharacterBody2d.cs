fb1ecb40d42e2e5c8d8bd7b5b91fcd2c2a119f70
Morgiliath <168931446+Morgiliath@users.noreply.github.com>
2024-11-09 20:02:39 -0500

    Tweaked Player Movement
    Adjusted movement variables
    Added Friction
    Fixed player looking at origin if not in motion

--- projects/tank-game/project/prefabs/player/CharacterBody2d.cs
+++ projects/tank-game/project/prefabs/player/CharacterBody2d.cs
@@ -3,9 +3,11 @@ using System;
 
 public partial class CharacterBody2d : CharacterBody2D
 {
-	public const float Speed = 200.0f;
+	public const float Speed = 50f;
+	public const float Friction = 0.9f;
 	public const float RotationSpeed = 2f;
 	public const float BulletSpeed = 400.0f;
+	private const float _epsilon = Single.Epsilon;
 	private float _rotationDirection;
 	private Vector2 _turretRotation;
 	private Vector2 _target;
@@ -14,6 +16,7 @@ public partial class CharacterBody2d : CharacterBody2D
 
 	private Vector2 WishDir;
 	private Sprite2D Turret;
+	private Sprite2D Tank;
 
 	[Signal]
 	public delegate void TankHitSomethingEventHandler(ulong CollidingObjectId);
@@ -22,6 +25,7 @@ public partial class CharacterBody2d : CharacterBody2D
 	{
 		bulletScene = GD.Load<PackedScene>("res://prefabs/bullet/Bullet.tscn");
 		this.Turret = GetNode<Sprite2D>("Turret");
+		this.Tank = GetNode<Sprite2D>("Tank");
 	}
 	public override void _Input(InputEvent @event)
 	{
@@ -50,16 +54,21 @@ public partial class CharacterBody2d : CharacterBody2D
 	public override void _PhysicsProcess(double delta)
 	{
 		// Update velocity
-		this.Velocity += this.WishDir;
-
-
-		// Update rotation
-		// TODO: Easing function
-		Rotation = _rotationDirection * RotationSpeed * (float)delta;
-		this.LookAt(this.Velocity*1000);
+		this.Velocity += Speed*this.WishDir;
+		if (this.Velocity.Length() > _epsilon)
+		{
+			this.Velocity = Friction*this.Velocity;
+			// Update rotation
+			// TODO: Easing function
+			Rotation = _rotationDirection * RotationSpeed * (float)delta;
+			if (this.WishDir.Length() != 0)
+			{
+				this.Tank.LookAt(this.Velocity*1000); 
+			}
+		}
+	
 		this.Turret.LookAt(_target);
 		_turretRotation = Position.DirectionTo(_target);
-
 		MoveAndSlide();
 	}
 
